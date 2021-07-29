69e788b2e1d5ab8da62436e270d2a3a09c158955
Sai Kartik <f20200435@pilani.bits-pilani.ac.in>
2021-07-29 17:57:45 +0530

    made the app talk to RTDB

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Controller',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -47,14 +47,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  DatabaseReference _W = FirebaseDatabase.instance.reference().child("W");
-  DatabaseReference _A = FirebaseDatabase.instance.reference().child("A");
-  DatabaseReference _S = FirebaseDatabase.instance.reference().child("S");
-  DatabaseReference _D = FirebaseDatabase.instance.reference().child("D");
-  DatabaseReference _LC = FirebaseDatabase.instance.reference().child("LC");
-  DatabaseReference _RC = FirebaseDatabase.instance.reference().child("RC");
+  DatabaseReference _w = FirebaseDatabase.instance.reference().child("W");
+  DatabaseReference _a = FirebaseDatabase.instance.reference().child("A");
+  DatabaseReference _s = FirebaseDatabase.instance.reference().child("S");
+  DatabaseReference _d = FirebaseDatabase.instance.reference().child("D");
+  DatabaseReference _lc = FirebaseDatabase.instance.reference().child("LC");
+  DatabaseReference _rc = FirebaseDatabase.instance.reference().child("RC");
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: PreferredSize(
@@ -71,93 +72,159 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               Row(
                 children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(65, 0, 0, 0),
-                    height: 75,
-                    width: 75,
-                    child: IconButton(
-                      icon: Image.asset('assets/images/up.jpg'),
-                      onPressed: () {
-                        print("ButtonState: Up");
-                      },
-                      splashColor: Colors.white,
-                      highlightColor: Colors.white,
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        _w.set(1);
+                      });
+                    },
+                    onTapCancel: () {
+                      setState(() {
+                        _w.set(0);
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(65, 0, 0, 0),
+                      height: 75,
+                      width: 75,
+                      child: IconButton(
+                        icon: Image.asset('assets/images/up.jpg'),
+                        onPressed: () {
+                          print("ButtonState: Up");
+                        },
+                        splashColor: Colors.white,
+                        highlightColor: Colors.white,
+                      ),
                     ),
                   ),
                   SizedBox(
                     width: 550,
                   ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    child: IconButton(
-                      icon: Image.asset('assets/images/Button.jpg'),
-                      onPressed: () {
-                        print("ButtonState: Button2");
-                      },
-                      splashColor: Colors.white,
-                      highlightColor: Colors.white,
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        _rc.set(1);
+                      });
+                    },
+                    onTapCancel: () {
+                      _rc.set(0);
+                    },
+                    child: Container(
+                      height: 75,
+                      width: 75,
+                      child: IconButton(
+                        icon: Image.asset('assets/images/Button.jpg'),
+                        onPressed: () {
+                          print("ButtonState: Button2");
+                        },
+                        splashColor: Colors.white,
+                        highlightColor: Colors.white,
+                      ),
                     ),
                   )
                 ],
               ),
               Row(
                 children: <Widget>[
-                  Container(
-                    height: 75,
-                    width: 75,
-                    child: IconButton(
-                      icon: Image.asset('assets/images/left.jpg'),
-                      onPressed: () {
-                        print("ButtonState: Left");
-                      },
-                      splashColor: Colors.white,
-                      highlightColor: Colors.white,
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        _a.set(1);
+                      });
+                    },
+                    onTapCancel: () {
+                      _a.set(0);
+                    },
+                    child: Container(
+                      height: 75,
+                      width: 75,
+                      child: IconButton(
+                        icon: Image.asset('assets/images/left.jpg'),
+                        onPressed: () {
+                          print("ButtonState: Left");
+                        },
+                        splashColor: Colors.white,
+                        highlightColor: Colors.white,
+                      ),
                     ),
                   ),
                   SizedBox(
                     width: 50,
                   ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    child: IconButton(
-                      icon: Image.asset('assets/images/right.jpg'),
-                      onPressed: () {
-                        print("ButtonState: Right");
-                      },
-                      splashColor: Colors.white,
-                      highlightColor: Colors.white,
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        _d.set(1);
+                      });
+                    },
+                    onTapCancel: () {
+                      setState(() {
+                        _d.set(0);
+                      });
+                    },
+                    child: Container(
+                      height: 75,
+                      width: 75,
+                      child: IconButton(
+                        icon: Image.asset('assets/images/right.jpg'),
+                        onPressed: () {
+                          print("ButtonState: Right");
+                        },
+                        splashColor: Colors.white,
+                        highlightColor: Colors.white,
+                      ),
                     ),
                   ),
                   SizedBox(
                     width: 350,
                   ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    child: IconButton(
-                      icon: Image.asset('assets/images/Button.jpg'),
-                      onPressed: () {
-                        print("ButtonState: Button1");
-                      },
-                      splashColor: Colors.white,
-                      highlightColor: Colors.white,
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        _lc.set(1);
+                      });
+                    },
+                    onTapCancel: () {
+                      setState(() {
+                        _lc.set(0);
+                      });
+                    },
+                    child: Container(
+                      height: 75,
+                      width: 75,
+                      child: IconButton(
+                        icon: Image.asset('assets/images/Button.jpg'),
+                        onPressed: () {
+                          print("ButtonState: Button1");
+                        },
+                        splashColor: Colors.white,
+                        highlightColor: Colors.white,
+                      ),
                     ),
                   )
                 ],
               ),
-              Container(
-                margin: EdgeInsets.fromLTRB(65, 0, 0, 0),
-                height: 75,
-                width: 75,
-                child: IconButton(
-                  icon: Image.asset('assets/images/down.jpg'),
-                  onPressed: () {
-                    print("ButtonState: Down");
-                  },
-                  splashColor: Colors.white,
-                  highlightColor: Colors.white,
+              GestureDetector(
+                onTapDown: (details) {
+                  setState(() {
+                    _s.set(1);
+                  });
+                },
+                onTapCancel: () {
+                  _s.set(0);
+                },
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(65, 0, 0, 0),
+                  height: 75,
+                  width: 75,
+                  child: IconButton(
+                    icon: Image.asset('assets/images/down.jpg'),
+                    onPressed: () {
+                      print("ButtonState: Down");
+                    },
+                    splashColor: Colors.white,
+                    highlightColor: Colors.white,
+                  ),
                 ),
               )
             ],
